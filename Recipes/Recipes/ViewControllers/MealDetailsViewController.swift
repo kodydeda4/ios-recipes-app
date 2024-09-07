07404caac0aefbe9e7e1913124e59f3401c6df51
Kody Deda <kodydeda4@gmail.com>
2024-09-07 14:14:07 -0400

    simplify recipe list

--- Recipes/Recipes/ViewControllers/MealDetailsViewController.swift
+++ Recipes/Recipes/ViewControllers/MealDetailsViewController.swift
@@ -26,6 +26,65 @@ final class MealDetailsViewController: CollectionViewController {
   }
 }
 
+// MARK: - ImageMarquee
+
+private final class ImageMarquee: UIView, EpoxyableView {
+
+  // MARK: Lifecycle
+
+  init(style: Style) {
+    self.style = style
+    super.init(frame: .zero)
+    contentMode = style.contentMode
+    clipsToBounds = true
+    addSubviews()
+    constrainSubviews()
+  }
+
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: Internal
+
+  struct Style: Hashable {
+    var height: CGFloat
+    var contentMode: UIView.ContentMode
+  }
+
+  struct Content: Equatable {
+    var imageURL: URL?
+  }
+
+  func setContent(_ content: Content, animated _: Bool) {
+    imageView.setURL(content.imageURL)
+  }
+
+  // MARK: Private
+
+  private let style: Style
+  private let imageView = UIImageView()
+
+  private func addSubviews() {
+    addSubview(imageView)
+  }
+
+  private func constrainSubviews() {
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    let heightAnchor = imageView.heightAnchor.constraint(equalToConstant: style.height)
+    heightAnchor.priority = .defaultHigh
+    NSLayoutConstraint.activate([
+      heightAnchor,
+      imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      imageView.topAnchor.constraint(equalTo: topAnchor),
+      imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
+  }
+
+}
+
+
 // MARK: - Previews
 
 struct MealDetailsViewController_Previews: PreviewProvider {
