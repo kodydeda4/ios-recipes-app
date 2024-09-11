6936f3e5ab80bac8a56419d51a5a919ac099925d
Kody Deda <kodydeda4@gmail.com>
2024-09-11 10:58:06 -0400

    add appicon

---
+++ Sources/Features/MealCategoryCollectionFeature/Internal/Array+chunked.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Array {
+  func chunked(into size: Int) -> [[Element]] {
+    guard size > 0 else { return [] }
+    return stride(from: 0, to: self.count, by: size).map {
+      Array(self[$0..<Swift.min($0 + size, self.count)])
+    }
+  }
+}
